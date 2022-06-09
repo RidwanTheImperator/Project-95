b2f4ccf71cdfd744ad46287630ddb63bb7452beb
RidwanTheImperator <92063949+RidwanTheImperator@users.noreply.github.com>
2022-06-08 20:04:22 -0400

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,45 +1,49 @@
 
 //ADD YOUR FIREBASE LINKS HERE
 const firebaseConfig = {
-      apiKey: "AIzaSyAhp1d2gQycN19v9DyPCxkpNI-osVQ3LRk",
-      authDomain: "rid-s-chat-app.firebaseapp.com",
-      databaseURL: "https://rid-s-chat-app-default-rtdb.firebaseio.com",
-      projectId: "rid-s-chat-app",
-      storageBucket: "rid-s-chat-app.appspot.com",
-      messagingSenderId: "277978332781",
-      appId: "1:277978332781:web:34fe1b3da2db7433208964"
+      apiKey: "AIzaSyA0KHtmZ9jVl_67Zk7XaMcGbmxypagUPFc",
+      authDomain: "rid-s-chat-apper.firebaseapp.com",
+      databaseURL: "https://rid-s-chat-apper-default-rtdb.firebaseio.com",
+      projectId: "rid-s-chat-apper",
+      storageBucket: "rid-s-chat-apper.appspot.com",
+      messagingSenderId: "60916336100",
+      appId: "1:60916336100:web:ac1fdf7f11d52aa2a033f1"
     };
-    
+
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
 
     user_name = localStorage.getItem("user")
-    document.getElementById("user_").innerHTML="Welcome "+user_name+"!"
+    document.getElementById("user_n").innerHTML="Welcome "+user_name+"!"
 
     function logout() {
-      localStorage.removeItem("user")
-      localStorage.removeItem("room_name")
-      window.location="index.html"
-}
+          localStorage.removeItem("user")
+          localStorage.removeItem("room_name")
+          window.location="index.html"
+    }
 
-function addroom() {
-      addrooms = document.getElementById("Add_room").value;
+    function addroom() {
+      addroomer = document.getElementById("Add_room").value;
 
-      firebase.database().ref("/").child(addrooms).update({
-            purp : "Da_Juker_Imperiumus"
+      firebase.database().ref("/").child(addroomer).update({
+            purp : "Da-juker"
       })
     }
 
-function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+function getData() {
+      firebase.database().ref("/").on('value', function(snapshot) {
+            document.getElementById("ouput").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
       //Start code
-      row = "<div class='room_name' id='"+Room_names+"onclick='chat_page(this.id)'>"+Room_names+"</div><hr>"
-      document.getElementById("output").innerHTML+=row
+      row = "<div class='room_name' id='"+Room_names+"onclick='chat_page(this.id)'>"+Room_names+"</div><br>"
+      document.getElementById("ouput").innerHTML+=row
       //End code
       });});}
-getData();
+      getData();
+
 
 function chat_page(name) {
-       localStorage.setItem("room_name", name);
-        window.location = "kwitter_page.html"; 
-     }
+       console.log(name);
+        localStorage.setItem("room_name", name);
+         window.location = "kwitter_page.html"; 
+      }
